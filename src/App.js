e5406e5748cb36ea6e6da8b9f539e0ba17519216
young24n <won8560@gmail.com>
2024-06-19 15:45:03 +0900

    로직 구현 input에서 enter시 btn 작동 편의성 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, { useState } from 'react';
 import './App.css';
+import professor from './professor';
 
 function App() {
+  const [name, setName] = useState('');
+  const [major, setMajor] = useState('검색하면 출력됨');
+
+  const handleSearch = () => {
+    const foundProfessor = professor.find((professor) => professor.name === name);
+    if (foundProfessor) {
+      setMajor(foundProfessor.major);
+    } else {
+      setMajor('그런 사람 여기 없수다');
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="App">
-      
+      <div className='you-need-margin50'>
+        <span>이름을 입력하시오: </span>
+        <input
+          type='text'
+          onChange={(e) => setName(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+        <button onClick={handleSearch}>확인</button>
+      </div>
+      <div>
+        <span>학과 : {major}</span>
+      </div>
     </div>
   );
 }
